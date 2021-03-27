7bed75b97f205ac8ba16b2b1ade69105a8a42d15
Athbi90 <Athbi90@gmail.com>
2021-03-27 07:36:08 +0300

    Sign in and sign up

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const passport = require("passport");
+//Routers Import
 const productsRoutes = require("./products/routers");
+const usersRoutes = require("./db/Users/routes");
+const { localStrategy } = require("./middleware/passport");
 const db = require("./db/models");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Passport setup
+app.use(passport.initialize());
+app.use(localStrategy);
+// Routers
 app.use("/", productsRoutes);
+app.user("/", usersRoutes);
 
 const run = async () => {
   try {
---
+++ src/db/Users/controllers.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../../db/models");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET, JWT_EXPIRATION_MS } = require("../../config/keys");
+
+exports.signup = async (req, res, next) => {
+  const { password } = req.body;
+  const saltRounds = 10;
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    req.body.password = hashedPassword;
+    const newUser = await User.create(req.body);
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.signin = async (req, res) => {
+  const { user } = req;
+  const payload = {
+    id: user.id,
+    username: user.username,
+    exp: Date.now() + parseInt(JWT_EXPIRATION_MS),
+  };
+  const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+  res.json({ token: token });
+};
